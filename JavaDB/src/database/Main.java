c34cd532cf36eecfe4b77cf11481b1a95726f2b1
Meng Vang <meng.vang92@yahoo.com>
2019-01-30 12:13:16 -0600

    query create a table

--- JavaDB/src/database/Main.java
+++ JavaDB/src/database/Main.java
@@ -2,14 +2,36 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// Calling getConnection function requires throws Exception for main method
-		getConnection();
+//		getConnection();
+		// Call the createTable function directly
+		createTable();
 	}
-	
+
+	// Creating a table
+	public static void createTable() throws Exception {
+		try {
+			// Call getConnection function to connect to database
+			Connection con = getConnection();
+			// Import PreparedStatement(java.sql)
+			// Create SQL query
+			PreparedStatement create = con.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS customers (id INT NOT NULL AUTO_INCREMENT, firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), PRIMARY KEY (id))");
+			// Run the query assigned to create
+			create.executeUpdate();
+		} catch (Exception e) {
+
+			System.out.println(e);
+		} finally {
+			System.out.println("Query ran successfully.");
+		}
+	}
+
 	// Establishes our database to MySQL
 	// Import Connection and add return statement
 	public static Connection getConnection() throws Exception {
@@ -21,14 +43,14 @@ public class Main {
 			String userName = "root";
 			String passWord = "root";
 			Class.forName(driver);
-			
+
 			Connection conn = DriverManager.getConnection(url, userName, passWord);
 			System.out.println("Connected");
 			return conn;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		return null;
 	}
 
