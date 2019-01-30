f35f954b1adf1331d247b5a7568a9f18bbb7e428
Meng Vang <meng.vang92@yahoo.com>
2019-01-30 11:39:08 -0600

    established MySQL connection

--- JavaDB/src/database/Main.java
+++ JavaDB/src/database/Main.java
@@ -1,10 +1,35 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		// Calling getConnection function requires throws exception for main method
+		getConnection();
+	}
+	
+	// Establishes our database to MySQL
+	// Import Connection and add return statement
+	public static Connection getConnection() throws Exception {
+		try {
+			String driver = "com.mysql.jdbc.Driver";
+			// CREATE DATABASE javaDB and USE javaDB in MySQL Workbench
+			String url = "jdbc:mysql://localhost:3306/javaDB";
+			// My credentials in MySQL Workbench
+			String userName = "root";
+			String passWord = "root";
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url, userName, passWord);
+			System.out.println("Connected");
+			return conn;
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		return null;
 	}
 
 }
